711a1eab43ed285486cd326cd4c5523d7dc0734a
Eric <charmjunewonder@gmail.com>
2015-01-24 11:09:14 -0500

    first commit

---
+++ Assets/MagnetController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagnetController : MonoBehaviour {
+	public GameObject sign;
+
+	public int quantityOfCharge = 1;
+	public bool typeOfCharge; //true - positive; false - nagetive
+
+	private GameObject nearestMagnet = null;
+
+	// Use this for initialization
+	void Start () {
+	 	LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        lineRenderer.SetColors(Color.yellow, Color.red);
+        lineRenderer.SetWidth(0.1F, 0.1F);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		DrawLineToNearestMagnet();
+		AddInstanceForce();
+	}
+
+	GameObject GetNearestMagnet(){
+		GameObject[] magnets = GameObject.FindGameObjectsWithTag("Magnet");
+		GameObject nearest = null;
+		float min = float.MaxValue;
+		for(int i = 0; i < magnets.Length; i++){
+			float distance = Vector3.Distance(magnets[i].transform.position, transform.position);
+			if(distance < min && !Mathf.Approximately(distance, 0)){
+				nearest = magnets[i];
+				min	= distance;
+			}
+		}
+		return nearest;
+	}
+
+	void DrawLineToNearestMagnet(){
+		LineRenderer lineRenderer = GetComponent<LineRenderer>();
+		nearestMagnet = GetNearestMagnet();
+		lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, nearestMagnet.transform.position);
+	}
+
+	void AddInstanceForce(){
+		Vector3 direction = nearestMagnet.transform.position - transform.position;
+		if(direction.magnitude < 1f) return;
+		float distance = Vector3.Distance(nearestMagnet.transform.position, transform.position);
+		float force = nearestMagnet.GetComponent<MagnetController>().quantityOfCharge * quantityOfCharge / (distance * distance);
+		rigidbody.AddForce(direction.normalized * force * 5, ForceMode.Force);
+	}
+}
